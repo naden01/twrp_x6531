53c4feb2a43d1b42c8f36ce497a1d5444d0ce3d2
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-10-14 09:11:55 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -55,15 +55,3 @@ then
 fi
 
 export LC_ALL="C"
-
-
-# Patches
-RET=0
-cd bootable/recovery
-git apply ../../device/tecno/LI9/patches/0001-Change-haptics-activation-file-path.patch > /dev/null 2>&1 || RET=$?
-cd ../../
-if [ $RET -ne 0 ];then
-    echo "ERROR: Patch is not applied! Maybe it's already patched?"
-else
-    echo "OK: All patched"
-fi
